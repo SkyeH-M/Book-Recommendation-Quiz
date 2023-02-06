33562e559127b24533971c5fc7429b1792b33a99
SkyeH-M <justskye98@gmail.com>
2023-02-06 12:23:27 +0000

    working on modal

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -471,7 +471,18 @@ function next() {
             }
         }
 
-    
+    // Modal
+    var modal = document.getElementById('myModal');
+    var span = document.getElementsByClassName('close')[0];
+    span.onclick = function() {
+        modal.style.display = 'none';
+    }
+
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = 'none';
+        }
+    }
 
        
 
