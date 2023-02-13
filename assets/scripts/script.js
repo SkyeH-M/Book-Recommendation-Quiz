14a2bb419c4ae62e03edf0a79b7ffad0b3634f16
SkyeH-M <justskye98@gmail.com>
2023-02-13 13:14:26 +0000

    Favicon added, and JS code formatted

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -23,19 +23,19 @@ D- Modern Fiction
 const GENRE_MAP = {
     A: {
         name: "Non-Fiction",
-        imgSrc: "assets/images/non-fiction.png"
+        imgSrc: "assets/images/non-fiction.png",
     },
     B: {
         name: "Horror",
-        imgSrc: "assets/images/horror.png"
+        imgSrc: "assets/images/horror.png",
     },
     C: {
         name: "Classics",
-        imgSrc: "assets/images/classics.png"
+        imgSrc: "assets/images/classics.png",
     },
     D: {
         name: "Modern Fiction",
-        imgSrc: "assets/images/modern-fiction.png"
+        imgSrc: "assets/images/modern-fiction.png",
     },
 };
 
@@ -43,96 +43,94 @@ let questions = [
     {
         question: "Which genre do you read most frequently?",
         answers: [
-            {option: "Non-Fiction", correspondingGenre:"A"},
-            {option: "Horror", correspondingGenre:"B"},
-            {option: "Classics", correspondingGenre:"C"},
-            {option: "Modern Fiction", correspondingGenre:"D"}
-            
-        ]
+            { option: "Non-Fiction", correspondingGenre: "A" },
+            { option: "Horror", correspondingGenre: "B" },
+            { option: "Classics", correspondingGenre: "C" },
+            { option: "Modern Fiction", correspondingGenre: "D" },
+        ],
     },
     {
         question: "Which of the following appeals most to you?",
         answers: [
-            {option: "I want to learn something", correspondingGenre:"A"},
-            {option: "I want to be intrigued", correspondingGenre:"B"},
-            {option: "I want to be transported to another time", correspondingGenre:"C"},
-            {option: "I want something that relates to my life", correspondingGenre:"D"}
-        ]
+            { option: "I want to learn something", correspondingGenre: "A" },
+            { option: "I want to be intrigued", correspondingGenre: "B" },
+            { option: "I want to be transported to another time", correspondingGenre: "C" },
+            { option: "I want something that relates to my life", correspondingGenre: "D" },
+        ],
     },
     {
         question: "Which of the following would be a good day out?",
         answers: [
-            {option: "Visiting a museum or gallery", correspondingGenre:"A"},
-            {option: "Exploring a haunted house", correspondingGenre:"B"},
-            {option: "Visiting a National Trust site", correspondingGenre:"C"},
-            {option: "Watching something new at the cinema", correspondingGenre:"D"}
-        ]
+            { option: "Visiting a museum or gallery", correspondingGenre: "A" },
+            { option: "Exploring a haunted house", correspondingGenre: "B" },
+            { option: "Visiting a National Trust site", correspondingGenre: "C" },
+            { option: "Watching something new at the cinema", correspondingGenre: "D" },
+        ],
     },
     {
         question: "Pick a quote you like...",
         answers: [
-            {option: '"Deviant men have been constructed as criminal, while deviant women have been constructed as insane."', correspondingGenre:"A"},
-            {option: '"What is worse: being locked outside of your own mind, or being locked inside of if?"', correspondingGenre:"B"},
-            {option: '"I do not think, therefore I am a moustache"', correspondingGenre:"C"},
-            {option: '"Wasn\’t friendship its own miracle, the finding of another person who made the entire lonely world seem somehow less lonely?"', correspondingGenre:"D"}
-        ]
+            { option: '"Deviant men have been constructed as criminal, while deviant women have been constructed as insane."', correspondingGenre: "A" },
+            { option: '"What is worse: being locked outside of your own mind, or being locked inside of if?"', correspondingGenre: "B" },
+            { option: '"I do not think, therefore I am a moustache"', correspondingGenre: "C" },
+            { option: '"Wasn’t friendship its own miracle, the finding of another person who made the entire lonely world seem somehow less lonely?"', correspondingGenre: "D" },
+        ],
     },
     {
         question: "You're buying a book for a friend, what do you choose?",
         answers: [
-            {option: "A book that explores an interest of theirs", correspondingGenre:"A"},
-            {option: "A great mystery to unravel", correspondingGenre:"B"},
-            {option: "A classic, they're classics for a reason!", correspondingGenre:"C"},
-            {option: "A book that inspired their favourite new TV show/film", correspondingGenre:"D"}
-        ]
+            { option: "A book that explores an interest of theirs", correspondingGenre: "A" },
+            { option: "A great mystery to unravel", correspondingGenre: "B" },
+            { option: "A classic, they're classics for a reason!", correspondingGenre: "C" },
+            { option: "A book that inspired their favourite new TV show/film", correspondingGenre: "D" },
+        ],
     },
     {
         question: "Which of these authors do you prefer?",
         answers: [
-            {option: "Gabor Maté", correspondingGenre:"A"},
-            {option: "Stephen King", correspondingGenre:"B"},
-            {option: "Virginia Woolf", correspondingGenre:"C"},
-            {option: "Sally Rooney", correspondingGenre:"D"}
-        ]
+            { option: "Gabor Maté", correspondingGenre: "A" },
+            { option: "Stephen King", correspondingGenre: "B" },
+            { option: "Virginia Woolf", correspondingGenre: "C" },
+            { option: "Sally Rooney", correspondingGenre: "D" },
+        ],
     },
     {
         question: "Pick a drink",
         answers: [
-            {option: "Water", correspondingGenre:"A"},
-            {option: "Whiskey", correspondingGenre:"B"},
-            {option: "Tea", correspondingGenre:"C"},
-            {option: "Cocktail", correspondingGenre:"D"}
-        ]
+            { option: "Water", correspondingGenre: "A" },
+            { option: "Whiskey", correspondingGenre: "B" },
+            { option: "Tea", correspondingGenre: "C" },
+            { option: "Cocktail", correspondingGenre: "D" },
+        ],
     },
     {
         question: "Imagine you're on holiday, what activity would you do?",
         answers: [
-            {option: "Check out local heritage sites", correspondingGenre:"A"},
-            {option: "Do an extreme sport or activity", correspondingGenre:"B"},
-            {option: "Visit somewhere that everyone recommends", correspondingGenre:"C"},
-            {option: "Chill on the beach or by the pool", correspondingGenre:"D"}
-        ]
+            { option: "Check out local heritage sites", correspondingGenre: "A" },
+            { option: "Do an extreme sport or activity", correspondingGenre: "B" },
+            { option: "Visit somewhere that everyone recommends", correspondingGenre: "C" },
+            { option: "Chill on the beach or by the pool", correspondingGenre: "D" },
+        ],
     },
     {
         question: "Choose a book you've loved in the past",
         answers: [
-            {option: "Women, Race & Class- Angela Davis", correspondingGenre:"A"},
-            {option: "We Need to Talk About Kevin- Lionel Shriver", correspondingGenre:"B"},
-            {option: "The Metamorphosis- Franz Kafka", correspondingGenre:"C"},
-            {option: "Normal People- Sally Rooney", correspondingGenre:"D"}
-        ]
+            { option: "Women, Race & Class- Angela Davis", correspondingGenre: "A" },
+            { option: "We Need to Talk About Kevin- Lionel Shriver", correspondingGenre: "B" },
+            { option: "The Metamorphosis- Franz Kafka", correspondingGenre: "C" },
+            { option: "Normal People- Sally Rooney", correspondingGenre: "D" },
+        ],
     },
     {
         question: "Pick a film",
         answers: [
-            {option: "13th", correspondingGenre:"A"},
-            {option: "The Descent", correspondingGenre:"B"},
-            {option: "12 Angry Men", correspondingGenre:"C"},
-            {option: "Everything Everywhere All at Once", correspondingGenre:"D"}
-        ]
-    }
+            { option: "13th", correspondingGenre: "A" },
+            { option: "The Descent", correspondingGenre: "B" },
+            { option: "12 Angry Men", correspondingGenre: "C" },
+            { option: "Everything Everywhere All at Once", correspondingGenre: "D" },
+        ],
+    },
 ];
-
 
 // create variables used to rep elements in our document, used to access them in the DOM:
 const restartBtn = document.getElementById("restart");
@@ -141,7 +139,6 @@ const answerOptions = Array.from(document.getElementsByClassName("option"));
 const questionText = document.getElementById("question-text");
 const usernameSubmit = document.getElementById("usernameSubmit");
 const textInput = document.getElementById("username");
-
 
 // Below code was suggested by Oisin from tutor support as a bug fix:
 let storedUsername;
@@ -152,26 +149,25 @@ function storeUsername() {
     sessionStorage.setItem("username", input);
     storedUsername = sessionStorage.getItem("username");
     document.getElementById("usernameForm").style.display = "none";
-};
+}
 
 // W3Schools- How to Trigger Button Click on Enter:
 let inputField = document.getElementById("username");
-    inputField.addEventListener("keypress", function(event) {
-        if (event.key === "Enter") {
-            event.preventDefault();
-            usernameSubmit.click();
-        }
-    });
+inputField.addEventListener("keypress", function (event) {
+    if (event.key === "Enter") {
+        event.preventDefault();
+        usernameSubmit.click();
+    }
+});
 
 let currentQuestionIndex = 0;
 let genreArray = [];
-
 
 function displayQuestion() {
     questionText.innerHTML = questions[currentQuestionIndex].question;
     answerOptions.forEach((eachOption, index) => {
         eachOption.style.background = "white";
-        eachOption.innerHTML =  questions[currentQuestionIndex].answers[index].option;
+        eachOption.innerHTML = questions[currentQuestionIndex].answers[index].option;
     });
 }
 
@@ -179,13 +175,13 @@ function initEventListeners() {
     restartBtn.addEventListener("click", restart);
     submitBtn.addEventListener("click", submit);
     answerOptions.forEach((eachOption, index) => {
-        eachOption.onclick =  () => {
+        eachOption.onclick = () => {
             genreArray.push(questions[currentQuestionIndex].answers[index].correspondingGenre);
-            if(currentQuestionIndex === 9) {
-                eachOption.style.background = '#5d6859';
+            if (currentQuestionIndex === 9) {
+                eachOption.style.background = "#5d6859";
             }
             displayNextQuestion();
-        }
+        };
     });
 }
 
@@ -195,9 +191,6 @@ function startQuiz() {
     displayQuestion();
 }
 
-
-
-
 // create function to reset current question index, remove hide class from elements, call startQuiz()
 function restart() {
     currentQuestionIndex = 0;
@@ -205,9 +198,9 @@ function restart() {
     submitBtn.classList.remove("hide");
     answerOptions.forEach((eachOption, index) => {
         eachOption.classList.remove("hide");
-        submitBtn.classList.add('hide');
+        submitBtn.classList.add("hide");
     });
-    
+
     // below hides recommendation img result after restarting quiz
     nfImg.classList.add("hide");
     sessionStorage.clear();
@@ -221,7 +214,7 @@ function displayNextQuestion() {
     if (currentQuestionIndex <= 9) {
         displayQuestion();
     }
-    
+
     if (currentQuestionIndex == 9) {
         showSubmitBtn();
     }
@@ -234,14 +227,14 @@ function submit() {
         answerOptions.forEach((eachOption, index) => {
             eachOption.classList.add("hide");
         });
-        displayGenre(); 
+        displayGenre();
     }
 }
 
 // hide submit button until quiz has been fully answered:
 function showSubmitBtn() {
     if (genreArray.length >= 9) {
-        submitBtn.classList.remove('hide');
+        submitBtn.classList.remove("hide");
     }
 }
 
@@ -249,12 +242,11 @@ function showSubmitBtn() {
 function mostFrequent(genreArray) {
     let maxCount = 0;
     let mostCommon;
-    
+
     for (let i = 0; i < genreArray.length; i++) {
         let count = 0;
         for (let j = 0; j < genreArray.length; j++) {
-            if (genreArray[i] === genreArray[j]) 
-            count++;
+            if (genreArray[i] === genreArray[j]) count++;
         }
         if (count > maxCount) {
             maxCount = count;
@@ -268,10 +260,10 @@ function mostFrequent(genreArray) {
 function displayGenre() {
     const selectedGenre = mostFrequent(genreArray);
     questionText.innerHTML = `Hi ${storedUsername}, you got ${GENRE_MAP[selectedGenre].name}! We recommend the following books...`;
-    let img = document.createElement('img');
+    let img = document.createElement("img");
     img.src = GENRE_MAP[selectedGenre].imgSrc;
     // why is this nfImg?
-   let nfImgSource = document.getElementById("nfImg");
+    let nfImgSource = document.getElementById("nfImg");
     nfImgSource.innerHTML = "";
     nfImgSource.appendChild(img);
     nfImg.classList.remove("hide");
@@ -279,11 +271,11 @@ function displayGenre() {
 
 /* Modal */
 
-let modal = document.getElementById('modalMain');
-let closeBtn = document.getElementById('closeBtn');
-closeBtn.addEventListener('click', closeModal);
+let modal = document.getElementById("modalMain");
+let closeBtn = document.getElementById("closeBtn");
+closeBtn.addEventListener("click", closeModal);
 
 function closeModal() {
-    modal.style.display = 'none';
+    modal.style.display = "none";
 }
 startQuiz();
